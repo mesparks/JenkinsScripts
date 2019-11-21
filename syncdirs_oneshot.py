9ac671b328536e5bb46ffe8d3409b9e773782531
Michael Sparks <michael.sparks@nextworld.net>
2019-11-21 14:26:11 -0700

    Changed logic to just sync root subdirectories

--- syncdirs_oneshot.py
+++ syncdirs_oneshot.py
@@ -24,7 +24,7 @@ src_bucket_name = args.srcbucket
 dst_bucket_name = args.dstbucket
 
 dst_directory = args.dstdir
-# dirs = []
+dirs = []
 root_excludes = ["*.js","*.html","*assets*","*.txt","*.png","*.woff","*.woff2","*.eot","*.ttf","*.css","*.svg"]
 exclude_dirs = ["*genapp*","assets/","dev/"]
 logging.info("Getting directories from bucket")
@@ -41,73 +41,69 @@ if src_bucket_region != dst_bucket_region:
     extra_args = extra_args + " " +  "--region %s --source-region %s" % (dst_bucket_region, src_bucket_region)
 
 
-def getdirs(bucket_name):
-    dirs = []
-    result = client.list_objects(Bucket=bucket_name, Delimiter='/')
-    for o in result.get('CommonPrefixes'):
-        dirs.append(o.get('Prefix'))
-    return dirs
 
 
+result = client.list_objects(Bucket=src_bucket_name, Delimiter='/')
+for o in result.get('CommonPrefixes'):
+    dirs.append(o.get('Prefix'))
 
-
-# exclude_line = ""
-# # Build Exclude Dir Line
-# for exclude in exclude_dirs:
-#     exclude_line = exclude_line + " " + "--exclude \"%s*\" " % exclude
-
-# # Build Exclude File Types
-# for exclude in root_excludes:
-#     exclude_line = exclude_line + " " + "--exclude \"%s\" " % exclude
-
-
-# include_line = ""
-
-
-def sync_subdir(subdir):
-    sync_cmd = ("aws s3 sync s3://%s/%s s3://%s/%s/%s  %s" % (src_bucket_name, subdir,dst_bucket_name,dst_directory, subdir, extra_args))
-    logging.info ("Running AWS Sync Command: %s" % sync_cmd)
-    if args.nowork == "true":
-        logging.info("Not doing any work, if you want to do work set --nowork to false")
-        return 0
-
-    # Keep a filecount for output
-    filescount = 0
-    p = subprocess.Popen(sync_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    while(True):
-        # returns None while subprocess is running
-        retcode = p.poll() 
-        line = p.stdout.readline()
-        logging.debug(line.decode("utf-8"))
-        if "error" in line.decode("utf-8"):
-            logging.error(line.decode("utf-8"))
-        elif "copy" in line.decode("utf-8"):
-            filescount = filescount + 1
-            if filescount % 500 == 0:
-                logging.info("Files copied: %d" % filescount)
-        else:
-            logging.info(line.decode("utf-8"))
-        # yield line
-        if retcode is not None:
-            break
-
-    logging.info("Copied %d files . . ." % filescount)
-    logging.info ("Ran AWS Sync Command: %s" % sync_cmd)
-    if retcode == 0:
-        logging.info ("Return Code: %d" % retcode)
-    else:
-        logging.error("Return Code: %d" % retcode)
-    return retcode
-
-
-
-
-dirs = getdirs(src_bucket_name)
 logging.info("Found Directories: %s", dirs)
+exclude_line = ""
+# Build Exclude Dir Line
+for exclude in exclude_dirs:
+    exclude_line = exclude_line + " " + "--exclude \"%s*\" " % exclude
+
+# Build Exclude File Types
+for exclude in root_excludes:
+    exclude_line = exclude_line + " " + "--exclude \"%s\" " % exclude
+
+
+include_line = ""
 # Build include line
 for dir in dirs:
     if dir not in exclude_dirs:
-        logging.info("Syncing directory %s" % dir)
-        ret_code = sync_subdir(dir)
-        if ret_code != 0:
-            sys.exit(ret_code)
+        for include in root_excludes:
+            include_line = include_line + " " + "--include \"%s%s\"" % (dir,include)
+
+
+sync_cmd = ("aws s3 sync s3://%s s3://%s/%s %s %s %s" % (src_bucket_name,dst_bucket_name,dst_directory,exclude_line,include_line, extra_args))
+logging.info ("Running AWS Sync Command: %s" % sync_cmd)
+if args.nowork == "true":
+    logging.info("Not doing any work, if you want to do work set --nowork to false")
+    sys.exit(0)
+# os.system(sync_cmd)
+
+ 
+# Keep a filecount for output
+
+filescount = 0
+
+p = subprocess.Popen(sync_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+while(True):
+    # returns None while subprocess is running
+    retcode = p.poll() 
+    line = p.stdout.readline()
+    logging.debug(line.decode("utf-8"))
+    if "error" in line.decode("utf-8"):
+        logging.error(line.decode("utf-8"))
+    elif "copy" in line.decode("utf-8"):
+        filescount = filescount + 1
+        if filescount % 500 == 0:
+            logging.info("Files copied: %d" % filescount)
+    else:
+        logging.info(line.decode("utf-8"))
+    # yield line
+    if retcode is not None:
+        break
+
+
+logging.info("Copied %d files . . ." % filescount)
+logging.info ("Ran AWS Sync Command: %s" % sync_cmd)
+if retcode == 0:
+    logging.info ("Return Code: %d" % retcode)
+else:
+    logging.error("Return Code: %d" % retcode)
+sys.exit(retcode)
+
+
+
